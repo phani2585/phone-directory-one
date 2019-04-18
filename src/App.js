3a93ce68be09501dfb7c9c02c93d85d50527f2d5
phani2585 <phani2585@gmail.com>
2019-04-18 11:01:33 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <div className="header">
-          Phone Directory
-      </div>
-        <button>Add</button>
-        <div>
-          <span>Name</span><br />
-          <span>PhoneNumber</span>
+import React, { Component } from 'react';
+import Header from './Header.js';
+
+
+ class App extends Component {	
+  render() {	 
+    return (	  
+      <div>	     
+          <Header/>	
+        <button>Add</button>	
         </div>
-        <label htmlFor="text">Name : </label>
-        <input type="text" placeholder="Type Here"/>
-      </div>
+        <span>Name</span><br/>
     );
   }
 }
 
-export default App;
+
+
+
