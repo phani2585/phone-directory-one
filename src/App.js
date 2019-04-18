840ae67d288e01fa06ea0f9bd3ea97bff9ad176e
phani2585 <phani2585@gmail.com>
2019-04-18 10:13:12 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        Phone Directory
+        <div className="header">
+          Phone Directory
+      </div>
+        <button>Add</button>
+        <div>
+          <span>Name</span><br />
+          <span>PhoneNumber</span>
+        </div>
+        <label htmlFor="text">Name : </label>
+        <input type="text" placeholder="Type Here"/>
       </div>
     );
   }
